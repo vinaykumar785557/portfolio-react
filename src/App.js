1506fa583dfa2bc29fd7aaae7cb9e9849cd9c879
vinaykumar785557 <c0785557@mylambton.ca>
2022-03-13 17:05:37 -0400

    routing and navbar is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+import Navbar from './components/Navbar'
 function App() {
-	return <div className='App'>Portfolio</div>;
+	return (
+		<div className='App'>
+      <Navbar/>
+			<Switch>
+				<Route path='/' exact component={Home}></Route>
+				<Route path='/projects'>
+					<Projects />
+				</Route>
+				<Route path='/skills'>
+					<Skills />
+				</Route>
+				<Route path='/contact'>
+					<Contact />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+import logo from '../avataaars.png';
+import { Link } from 'react-router-dom';
+import { MenuSharp } from '@mui/icons-material';
+
+function Navbar() {
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+	const [toggleMenu, setToggleMenu] = useState(false);
+
+	console.log(screenWidth);
+	const toggleNav = () => {
+		setToggleMenu(!toggleMenu);
+	};
+
+	useEffect(() => {
+		const changeWidth = () => {
+			setScreenWidth(window.innerWidth);
+		};
+
+		window.addEventListener('resize', changeWidth);
+
+		return () => {
+			window.removeEventListener('resize', changeWidth);
+		};
+	}, []);
+
+	return (
+		<div className='navbar'>
+			<div className='navbar__leftside'>
+				<Link to='/'>
+					<img src={logo} alt='image-avatar'></img>
+				</Link>
+			</div>
+
+			<div className='navbar__rightside'>
+				{(toggleMenu || screenWidth > 600) && (
+					<nav className='navlist'>
+						<ul className='navitems'>
+							<li>
+								<Link to='/'>Home</Link>
+							</li>
+							<li>
+								<Link to='/projects'>Projects</Link>
+							</li>
+							<li>
+								<Link to='/skills'>Skills</Link>
+							</li>
+							<li>
+								<Link to='/contact'>Contact</Link>
+							</li>
+						</ul>
+					</nav>
+				)}
+
+				<div className='buttonwrapper'>
+					<button onClick={toggleNav}>
+						<MenuSharp />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
